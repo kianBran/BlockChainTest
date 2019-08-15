aa23207ff866753277f012a4177ce9ad09c2eda4
kian <m15818511493@163.com>
2019-08-15 18:27:44 +0800

    pow證明

--- BlockChain.go
+++ BlockChain.go
@@ -1,4 +1,4 @@
-package main
+package proofofwork
 
 import (
 	_ "crypto/sha256"
---
+++ proofoffwork.go
@@ -0,0 +1,30 @@
+package proofofwork
+
+import "math/big"
+
+type proofofwork struct {
+	//a、block
+	block*Block
+	//b、目標值
+	//一個非常大的數，他有很多方法
+	target*big.Int
+}
+
+//提供創建pow的函數
+func NewProofofWork(block *Block) *ProofofWork {
+	pow:=proofofwork{
+		block:block,
+	}
+
+	//我們指定的難度值，現在是一個string類型，需要進行轉換
+	targetStr:="0000100000000000000000000000000000000000000000000000000000000000"
+
+	//引入輔助變量，目的是將上面的難度值轉成big.int
+	tmpInt:=big.Int{}
+	//將難度值複製給big。int,指定16進制格式
+	tmpInt.SetString(targetStr,16)
+
+	pow.target=&tmpInt
+	return &pow
+}
+
