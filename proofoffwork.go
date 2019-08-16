88bea661dd98ae1cf78e0cc378f2404851b7adc2
kian <m15818511493@163.com>
2019-08-16 11:39:13 +0800

    挖礦成功

--- proofoffwork.go
+++ proofoffwork.go
@@ -39,36 +39,39 @@ func (pow *ProofofWork)Run() ([]byte,uint64) {
 	var nonce uint64
 	var hash [32]byte
 	//1、拼装数据(区块的数据，还有不断变化的随机数)
-	tmp:=[][]byte{
-		Uint64ToByte(block.Version),
-		block.PrvHash,
-		block.MerkelRoot,
-		Uint64ToByte(block.TimeStamp),
-		Uint64ToByte(block.Difficulty),
-		Uint64ToByte(nonce),
-		block.Data,
+	for{
+		tmp:=[][]byte{
+			Uint64ToByte(block.Version),
+			block.PrvHash,
+			block.MerkelRoot,
+			Uint64ToByte(block.TimeStamp),
+			Uint64ToByte(block.Difficulty),
+			Uint64ToByte(nonce),
+			block.Data,
+		}
+
+		blockInfo := bytes.Join(tmp, []byte{})
+		//2、做哈希运算
+		hash = sha256.Sum256(blockInfo)
+		//3、与pow中的target进行比较
+		tmpInt:=big.Int{}
+		tmpInt.SetBytes(hash[:])
+
+		if tmpInt.Cmp(pow.target)==-1{
+			//a、找到了。退出返回
+			fmt.Printf("挖矿成功! hash:%x,nonce:%d\n",hash,nonce)
+			return hash[:],nonce
+		}else {
+			//b、没找到，继续找，随机数加1
+			nonce++
+		}
+
+
+
+		//return []byte("HelloWorld"),10
+		//return hash[:],nonce
 	}
 
-	blockInfo := bytes.Join(tmp, []byte{})
-	//2、做哈希运算
-	hash = sha256.Sum256(blockInfo)
-	//3、与pow中的target进行比较
-	tmpInt:=big.Int{}
-	tmpInt.SetBytes(hash[:])
-
-	if tmpInt.Cmp(pow.target)==-1{
-		//a、找到了。退出返回
-		fmt.Printf("挖矿成功 hash=%x\n nonce=%d\n",hash,nonce)
-		return nil, 0
-	}else {
-		//b、没找到，继续找，随机数加1
-		nonce++
-	}
-
-
-
-	//return []byte("HelloWorld"),10
-	return hash[:],nonce
 
 }
 
