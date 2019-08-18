db9531f48b96851cee59ba29b09ba8e303d1ed68
kian <m15818511493@163.com>
2019-08-18 23:19:51 +0800

    完善挖矿交易

--- transaction.go
+++ transaction.go
@@ -7,6 +7,8 @@ import (
 	"log"
 )
 
+const reword=12.5
+
 //定义交易结构
 type Transaction struct {
 	TXID []byte //交易Id
@@ -18,7 +20,7 @@ type Transaction struct {
 type TXInput struct {
 	//引用的交易ID
 	TXid []byte
-	//引用的交易输出索引值
+	//引用的output索引值
 	Index int64
 	//解锁脚本，我们用地址来模拟
 	Sig string
@@ -43,5 +45,22 @@ func (tx *Transaction) Sethash()  {
 	data:=buffer.Bytes()
 	hash:=sha256.Sum256(data)
 	tx.TXID=hash[:]
+
+}
+
+//提供创建交易方法（挖矿交易）
+func NewCoinBaseTX(address string,data string) *Transaction {
+	//挖矿交易的特点
+	//1、只有一个input
+	//2、无需引用交易id
+	//3、不需要index
+	//旷工由于挖矿时无需指定签名，所以这个sig字段由矿工自由填写数据，一般是填写矿池的名字
+	input:=TXInput{[]byte{},-1,data}
+	output:=TXOput{reword,address}
+
+	tx:=Transaction{[]byte{},[]TXInput{input},[]TXOput{output}}
+	tx.Sethash()
+
+	return &tx
 
 }
